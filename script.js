7ce3f6ebd707372be08c83d00638d27aa31d03da
marilynh7 <mehernandez@cpp.edu>
2021-06-10 10:51:25 -0700

    Trending page is working

--- script.js
+++ script.js
@@ -16,8 +16,6 @@ async function getResults(event){
    console.log(input);
  
    const apiURL = "https://api.themoviedb.org/3/search/movie?api_key="+apiKey+"&language=en-US&query="+input+"&page=1&include_adult=false";
-  // const imgURL =
-   console.log
    const response = await fetch(apiURL);
    const responseData = await response.json();
    console.log(responseData);
@@ -31,7 +29,9 @@ function displayResults(movieData){
    //console.log(movieData.backdrop_path);
    let moviePoster = movieData.poster_path;
    let movieTitle = movieData.original_title;
+   let movieRating = movieData.vote_average;
    console.log(movieTitle);
+   console.log(movieRating);
    let posterSrc = "https://image.tmdb.org/t/p/original/"+moviePoster;
    //console.log(posterSrc);
    movieArea.innerHTML += `
@@ -39,7 +39,10 @@ function displayResults(movieData){
 
       <img id = "poster" src="${posterSrc}" alt="Movie Poseter"> </img>
       <h4 id= "titles">${movieTitle} </h4>
-
+      <div id="ratings">
+         <p>&#11088;</p>
+         <h3 id= "rating">${movieRating} </h3>
+      </div>
    </div>    
    `
 }
@@ -50,4 +53,37 @@ function clearHTML(){
 
    `
 }
+
+const trendingArea = document.querySelector("#movieArea");
+//https://api.themoviedb.org/3/movie/now_playing?api_key=<<api_key>>&language=en-US&page=1
+//display the trending movies on the main page 
+async function getTrending(){
+   const playingNow = "https://api.themoviedb.org/3/movie/now_playing?api_key="+apiKey+"&language=en-US&page=1";
+   let response = await fetch(playingNow);
+   let responseData = await response.json();
+   console.log(responseData);
+   responseData.results.forEach((el) => displayTrending(el));
+}
+
+
+function displayTrending(movieData){
+   let moviePoster = movieData.poster_path;
+   let movieTitle = movieData.original_title;
+   let movieRating = movieData.vote_average;
+   let posterSrc = "https://image.tmdb.org/t/p/original/"+moviePoster;
+
+   trendingArea.innerHTML += `
+      <div id="movieInfo">
+         <img id="poster" src="${posterSrc}" alt="Trending Movie Poster"> </img>
+         <h4 id = "titles">${movieTitle}</h4>
+         <div id="ratings">
+            <p>&#11088;</p>
+            <h3 id=rating>${movieRating}</h3>
+         </div>
+      </div>
+   `
+
+}
+
+getTrending();
 
