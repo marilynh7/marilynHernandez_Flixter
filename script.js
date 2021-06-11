6866517036a15da70685a01e25e7bb9a6a347081
marilynh7 <mehernandez@cpp.edu>
2021-06-11 11:21:22 -0700

    Needs one more feature

--- script.js
+++ script.js
@@ -2,18 +2,23 @@
 const apiKey = "011647eee57bc654602e870e8f6281ce";
 const movieForm = document.querySelector("form");
 const movieArea = document.querySelector("#movieArea");
+const search = document.querySelector("#search");
 //when you click the search button movie results will appear 
-movieForm.addEventListener("submit", getResults);
-
+//.addEventListener("submit", getResults);
+search.addEventListener("input",getResults);
 //get the seach movie data from the API and call displayResults
 async function getResults(event){
    event.preventDefault();
    //clears the page after every search 
    clearHTML();
    //looks at the search box 
-   const userInput = event.target.search;
+   const input = event.target.value;
    //gets the value inputted in the search bar 
-   const input = userInput.value;
+   //const input = userInput.value;
+   if(!input){
+      getTrending(trendingApiPage);
+      const reloadtButton = document. querySelector("#reload");
+   }
    const apiURL = "https://api.themoviedb.org/3/search/movie?api_key="+apiKey+"&language=en-US&query="+input+"&page=1&include_adult=false";
    //Gets data from the API 
    const response = await fetch(apiURL);
@@ -25,6 +30,7 @@ async function getResults(event){
 //dynaminaclly adds the searched movie results to HTML 
 function displayResults(movieData){
 
+
    let moviePoster = movieData.poster_path;
    let movieTitle = movieData.original_title;
    let movieRating = movieData.vote_average;
@@ -32,7 +38,7 @@ function displayResults(movieData){
    movieArea.innerHTML += `
    <div id="movieInfo">
 
-      <img id = "poster" src="${posterSrc}" alt="Movie Poseter"> </img>
+      <img class = "poster" src="${posterSrc}" alt="Movie Poseter"> </img>
       <h4 id= "titles">${movieTitle} </h4>
       <div id="ratings">
          <p>&#11088;</p>
@@ -66,10 +72,11 @@ function displayTrending(movieData){
    let movieTitle = movieData.original_title;
    let movieRating = movieData.vote_average;
    let posterSrc = "https://image.tmdb.org/t/p/original/"+moviePoster;
+   let id = movieData.id;
    console.log(trendingApiPage);
    trendingArea.innerHTML += `
       <div id="movieInfo">
-         <img id="poster" src="${posterSrc}" alt="Trending Movie Poster"> </img>
+         <img class="poster" src="${posterSrc}" alt="Trending Movie Poster" name="${id}" > </img>
          <h3 id = "titles">${movieTitle}</h3>
          <div id="ratings">
             <p>&#11088;</p>
@@ -94,6 +101,35 @@ async function loadMoreClick(event){
 
 loadButton.addEventListener('click',loadMoreClick);
 
+ document.body.addEventListener('click', (event)=>{
+    info(event);
+ })
+
+
+ async function info(event){
+    if(event.target.matches(".poster")){
+       var id = event.target.name;         //holds the id of my movie
+       console.log(id);
+       const movieURL = "https://api.themoviedb.org/3/movie/"+id+"?api_key="+apiKey+"&language=en-US";
+       const response = await fetch(movieURL);
+       const responseData = await response.json();
+       displayOverview(responseData);
+    }
+ }
+
+ const infoArea = document.querySelector("#infoPage");
+ function displayOverview(data){
+    console.log(data.overview);
+    infoArea.innerHTML += `
+    <div id="popBox">
+      
+      <h3>"${data.overview}"</h3>
+    </div>
+      
+    `
+
+ }
+  
 
 
 
